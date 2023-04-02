9d551854a436652dd80b38f410bc8fa5020efdcc
Harun Doğdu <Harundogdu06@gmail.com>
2023-04-03 01:20:18 +0300

    feat: users who enter from the browser api-docs page will be redirected to the new api-docs page

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 
 import cors from "cors";
 import express, { Request, Response } from "express";
+import swagger from "swagger-ui-express";
 
 import { router as authorRouter } from "@/author/author.router";
 import { router as bookRouter } from "@/book/book.router";
@@ -16,6 +17,8 @@ if (!process.env.PORT) {
 
 const PORT: number = parseInt(process.env.PORT, 10);
 const API_PREFIX: string = process.env.API_PREFIX || "/api/v1";
+import swaggerDocument from "@/lib/swagger.json";
+import browserList from "@/lib/browsers.json";
 
 const app = express();
 
@@ -24,6 +27,23 @@ app.use(express.json());
 
 app.use(`${API_PREFIX}/authors`, authorRouter);
 app.use(`${API_PREFIX}/books`, bookRouter);
+app.use(`${API_PREFIX}/docs`, swagger.serve, swagger.setup(swaggerDocument));
+
+app.get(API_PREFIX, (req: Request, res: Response) => {
+  if (
+    req.headers["user-agent"] &&
+    browserList.some((browser) =>
+      req.headers["user-agent"]?.includes(browser.name)
+    )
+  ) {
+    res.redirect(`${API_PREFIX}/docs`);
+  }
+
+  res.json({
+    message: "Welcome to the Bookstore API",
+    documentation: `${API_PREFIX}/docs`,
+  });
+});
 
 app.use((req: Request, res: Response) => {
   errorHandler.handleError(new Error("Not found"), req, res);
